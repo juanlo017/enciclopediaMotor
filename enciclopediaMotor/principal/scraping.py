2f82cc3f849822bda937438e3921497065e8ac78
Juan Lopez <juanquiroslopez@gmail.com>
2024-01-04 19:36:48 +0100

    arreglado imagenes

--- enciclopediaMotor/principal/scraping.py
+++ enciclopediaMotor/principal/scraping.py
@@ -1,23 +1,24 @@
+import time
 from bs4 import BeautifulSoup
 import requests
-
-# SCRAPING FROM WEB:https://www.coches.net/nuevo/km-0/, 
-# https://www.coches.net/nuevo/km-0/?pg=2 HASTA 
-# https://www.coches.net/nuevo/km-0/?pg=289
+from django.conf import settings
 import os, ssl
+from fake_useragent import UserAgent
 
+# SCRAPING FROM WEB: https://www.wikidex.net/wiki/WikiDex
+ua = UserAgent()
 
 def get_pokemons():
     pokemon_list = []
     if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
         ssl._create_default_https_context = ssl._create_unverified_context
-
-    soup = BeautifulSoup(requests.get('https://www.wikidex.net/wiki/Lista_de_Pokémon').text, "lxml")
+    headers = {'User-Agent': ua.random}
+    soup = BeautifulSoup(requests.get('https://www.wikidex.net/wiki/Lista_de_Pokémon', headers=headers).text, "lxml")
 
     national_pokedexes = soup.find_all('table', class_='tabpokemon sortable mergetable')
     for pokedex in national_pokedexes:
         pokemon_rows = pokedex.find_all('tr')
-        for pokemon_row in pokemon_rows:
+        for pokemon_row in pokemon_rows[20:25]:
             pokemon = pokemon_row.find_all('td')
             
             if len(pokemon) == 4:
@@ -34,12 +35,15 @@ def get_pokemons():
 
 
 def get_pokemon_details(url):
-    soup = BeautifulSoup(requests.get(url).text, "lxml")
+    headers = {'User-Agent': ua.random}
+    soup = BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
     pokemon_details = soup.find('div', class_='cuadro_pokemon')
 
     name = pokemon_details.find('div', class_='titulo').text
-    picture_url = pokemon_details.find('div', class_='vnav_datos').find('a', class_='image').attrs['href']
-    complete_picture_url = f"https://www.wikidex.net/{picture_url}"
+    picture_url = pokemon_details.find('div', class_='vnav_datos').find('a', class_='image').find('img')['src']
+    
+    picture_path = download_picture_to_static(picture_url, name)
+
     number = pokemon_details.find('span', id='numeronacional').text
     
     tabla_datos = pokemon_details.find('table', class_='datos resalto')
@@ -64,7 +68,8 @@ def get_pokemon_details(url):
     description = soup.find('table', class_='pokedex radius10 tfx').find_all('tr')[1].find_all('td')
     description = ''.join(el.text for el in description).replace('\n','')
 
-    pokemon = [name, number, generation, list_types, weight, height, color, description, complete_picture_url]
+    pokemon = [name, number, generation, list_types, weight, height, color, description, picture_path]
+    """ time.sleep(1) # To avoid being banned """
     return pokemon
 
 def save_scrapped_data_to_file():
@@ -79,3 +84,21 @@ def save_scrapped_data_to_file():
                     f.write(','.join(att) + ';')
             f.write('\n')
     return len(pokemons)
+
+def download_picture_to_static(url, picture_name):
+    headers = {'User-Agent': ua.random}
+    response = requests.get(url,headers=headers)
+
+    if response.status_code == 200:
+        # Ensure the path to the static folder is correct
+        static_path = os.path.join(settings.BASE_DIR, 'principal','static', 'img', f"{picture_name}.png")
+        res = os.path.join('img', f"{picture_name}.png")
+        if(os.path.exists(static_path)): 
+            static_path = os.path.join(settings.BASE_DIR, 'principal','static', 'img', f"{picture_name}_2.png")
+            res = os.path.join('img', f"{picture_name}_2.png")
+        # Writing the image to the file
+        with open(static_path, 'wb') as file:
+            file.write(response.content)
+        return res
+    else:
+        print( "Failed to download the image")
